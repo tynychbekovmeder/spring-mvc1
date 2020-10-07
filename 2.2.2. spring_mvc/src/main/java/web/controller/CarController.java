76cd27e73960832d81b7e986af305b63136efd52
ergeshmametuulukanat <ergeshmametuulukanat@gmail.com>
2020-10-07 21:31:53 +0600

    added car

---
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.config.CarService;
+import web.config.model.Car;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/cars")
+public class CarController {
+    private CarService carService = new CarService();
+
+    @GetMapping
+    public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
+        List<Car> cars = carService.getCars(count);
+        model.addAttribute("cars", cars);
+        return "cars";
+    }
+}
